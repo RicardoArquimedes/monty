50f4303cefbd8ff8a7dc8cc26de745a1ba4cdc1e
RicardoArquimedes <richardcqt28@gmail.com>
2020-09-17 15:14:19 +0000

    add matching and monty function

---
+++ match_funct.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+/**
+ * get_code_fn - Pointer to function
+ * acording input opcode
+ * @opcode: String with name of the opcode
+ *
+ * Return: Void
+ */
+void (*get_funct(char *opcode))(stack_t **stack, unsigned int number_line)
+{
+	instruction_t ins_code[] = {
+		{"push", push_funct},
+		{"pall", pall_funct},
+		{"pint", pint_funct},
+		{NULL, NULL}
+	};
+	int i = 0, line = 0;
+	char *code = NULL;
+
+	i = 0;
+	while (i < 3)
+	{
+		if (strcmp(opcode, ins_code[i].opcode) == 0)
+		{
+			return (ins_code[i].f);
+		}
+		i++;
+	}
+	line = global_line.number_line;
+	code = global_line.opcode;
+	dprintf(STDERR_FILENO, "L%d: unknown instruction %s\n", line, code);
+	exit(EXIT_FAILURE);
+}
---
+++ monty_funct.c
@@ -0,0 +1,50 @@
+
+#include "monty.h"
+/**
+ * script_monty - This function split the input file in line  and arguments
+ * and later invoke the function acording opcode input.
+ * @input: File to read.
+ *
+ * Return: Void
+ */
+void script_monty(FILE *input)
+{
+	char *line = NULL;
+	char *argument = NULL;
+	char str[CHARMAX];
+	stack_t *head = NULL;
+	int i = 1, j = 0;
+
+	global_line.file_i = input;
+	global_line.opcode = NULL;
+	line = fgets(str, CHARMAX, input);
+	while (!feof(input))
+	{
+
+		argument = strtok(line, " \t\n");
+		j = 0;
+		while (argument && j < 2)
+		{
+			if (j == 0)
+			{
+				if (argument[0] == '#')
+				{
+					global_line.opcode = "#"; /* safe argument*/
+					i--;
+				}
+				else
+					global_line.opcode = argument; /* safe argument*/
+				global_line.number_line = i;	       /*safe number_line */
+			}
+			if (j == 1)
+				global_line.argument = argument;
+			argument = strtok(NULL, " \t\n"), j++;
+		}
+		if (global_line.opcode)
+			get_funct(global_line.opcode)(&head, global_line.number_line);
+		global_line.argument = NULL;
+		global_line.opcode = NULL, i++;
+		line = fgets(str, CHARMAX, input);
+	}
+	free_dlistint(head), fclose(input);
+}
