ae4ad4688e8157403eb95253cac77b91c0bbc13d
RicardoArquimedes <richardcqt28@gmail.com>
2020-09-17 18:24:01 +0000

    adding opcode nop

---
+++ nop_funct.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop_funct - doesnt do anything
+ * @stack: pointer to head of stack
+ * @line_number: number of the line in the bytecode file
+ * Return: number of nodes in the linked list, 0 if empty
+ */
+void nop_funct(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
